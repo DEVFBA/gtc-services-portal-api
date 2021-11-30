59d047ee0776db33a31aae1b69339d4357a4b646
alexisherolv <alexisherolv@gmail.com>
2021-11-29 18:26:34 -0600

    Cambios Auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,24 +1,46 @@
 var jwt = require('jsonwebtoken');
 var express = require('express');
 var config = require('../configs/config');
+const configDataBase = require("../dbconfig");
+const sql = require('mssql');
 
 const auth = express.Router(); 
 auth.use(async (req, res, next) => {
     const token = req.headers['access-token']; 
     var secret = await config.getSecret()
     if (token) {
-      jwt.verify(token, secret, (err, decoded) => {      
+      jwt.verify(token, secret, async (err, decoded) => {      
         if (err) {
-          return res.json({ mensaje: 'Token inválida' });    
+          return res.status(401).json( { 
+            error: {
+              mensaje: 'Token inválida.' 
+            }
+          } ); 
         } else {
-          req.decoded = decoded;    
-          next();
+          const pool = await sql.connect(configDataBase);
+          const validUser = await pool.request()
+            .input('pvOptionCRUD', sql.VarChar, 'R')
+            .input('pvIdUser', sql.VarChar, decoded.id)
+            //.input('pvIdUser', sql.VarChar, 'decoded.userName')
+            .execute('spSecurity_Users_CRUD_Records');
+          
+          if( validUser.recordset.length === 0 || !validUser.recordset[0].Status ){
+            return res.status(401).json( { 
+              error: {
+                mensaje: 'Token inválida' 
+              }
+            } ); 
+          } else {
+            next();
+          }
         }
       });
     } else {
-      res.send({ 
+      return res.status(401).json( { 
+        error: {
           mensaje: 'Token no proveída.' 
-      });
+        }
+      } ); 
     }
 });
 
