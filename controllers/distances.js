dfbd1eef600d22d13c3a32649101cee3f3165d0e
alexisherolv <alexisherolv@gmail.com>
2022-02-17 11:46:49 -0600

    Cambios log

--- controllers/distances.js
+++ controllers/distances.js
@@ -306,30 +306,77 @@ async function getDistanceWT(req){
 
       //PASO 3: Hacemos el llamado a la API de Geocode
       const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${cadena3}&key=${googleAPIKey}`
+      console.log(url)
       try{
         let response = await axios({
             method: 'get',
             url: url,
             json: true
         })
-        //PASO 3: Guardamos latitud y longitud en OrigenC
-        origenC = {
-          latitud: response.data.results[0].geometry.location.lat,
-          longitud: response.data.results[0].geometry.location.lng,
-          identificadorCliente: origen.identificadorCliente
-        }
-
-        urlDistances = urlDistances + response.data.results[0].geometry.location.lat
-        urlDistances = urlDistances + "%2C" + response.data.results[0].geometry.location.lng + "&destinations="
-      } catch(err){
-          return {
-            error: err
+        if(response.data.status === "ZERO_RESULTS")
+        {
+          origenC = {
+            latitud: "0",
+            longitud: "0",
+            identificadorCliente: origen.identificadorCliente
           }
+  
+          urlDistances = urlDistances + "0"
+          urlDistances = urlDistances + "%2C" + "0" + "&destinations="
+        }
+        else {
+          origenC = {
+            latitud: response.data.results[0].geometry.location.lat,
+            longitud: response.data.results[0].geometry.location.lng,
+            identificadorCliente: origen.identificadorCliente
+          }
+  
+          urlDistances = urlDistances + response.data.results[0].geometry.location.lat
+          urlDistances = urlDistances + "%2C" + response.data.results[0].geometry.location.lng + "&destinations="
+        }
+        //PASO 3: Guardamos latitud y longitud en OrigenC
+      } catch(err){
+        return {
+          mensaje: "ERROR: Error al obtener COORDENADAS de la dirección " + cadena3 + ", Valide",
+          error: err.toString() 
+        }
       }
     }
     console.log("Origen " + i + ": " + origenC.latitud + ", " + origenC.longitud)
     //Guardamos los origenes
     origenesC[i] = origenC
+
+    /*var y=0;
+    var destinosFlag = false
+    while(destinosFlag === false)
+    {
+        var z;
+        if(y===0)
+        {
+            z=0
+        }
+        else {
+            z = y*25
+        }
+        var destinations = []
+        var destinationsCount = 0
+        var countC=0
+       
+        while(z< destinos.length && countC<25)
+        {
+           destinations[destinationsCount] = destinos[z].Price
+           destinationsCount++
+           z++
+           countC++
+        }
+        //console.log(data)
+
+        if(z >= destinos.length)
+        {
+          destinosFlag = true
+        }
+        y++
+    }*/   
 
     //Para sacar los destinos
     var destinos = distances.distancias[i].destinos
@@ -363,13 +410,20 @@ async function getDistanceWT(req){
         countCoordenadas++
         //PASO 1: Sustituimos espacios por "+" en la direccion
         var cadena = destino.direccion
-        var cadena2 = cadena.replace(/ /g, "+")
-
+        var cadena2 = cadena.replace(/–/g, " ")
+        //Quitamos guiones
+        var cadena3 = cadena2.replace(/ /g, "+")
+        //Quitamos signo #
+        var cadena4 = cadena3.replace(/#/g, "");
+         //Quitamos espacios en blanco de la cadena
+        var cadena5 = cadena4.replace(/  /g, "") 
         //PASO 2: Quitamos acentos o caracteres no deseados a la cadena
-        var cadena3 = cadena2.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+        var cadena6 = cadena5.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+        console.log(cadena6)
 
         //PASO 3: Hacemos el llamado a la API de Geocode
-        const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${cadena3}&key=${googleAPIKey}`
+        const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${cadena6}&key=${googleAPIKey}`
+        console.log(url)
         try{
           let response = await axios({
               method: 'get',
@@ -377,32 +431,52 @@ async function getDistanceWT(req){
               json: true
           })
           //PASO 3: Guardamos latitud y longitud en destinosC
-          destinosC[y] = {
-            latitud: response.data.results[0].geometry.location.lat,
-            longitud: response.data.results[0].geometry.location.lng,
-            identificadorCliente: destino.identificadorCliente
-          }
-          if(destinos[y+1]!==undefined)
+          console.log(response.data.status)
+          if(response.data.status === "ZERO_RESULTS")
           {
-            urlDistances = urlDistances + response.data.results[0].geometry.location.lat
-            urlDistances = urlDistances + "%2C" + response.data.results[0].geometry.location.lng + "%7C"
+            destinosC[y] = {
+              latitud: origenesC[i].latitud,
+              longitud: origenesC[i].longitud,
+              identificadorCliente: destino.identificadorCliente
+            }
+            if(destinos[y+1]!==undefined)  
+            {
+              urlDistances = urlDistances + origenesC[i].latitud
+              urlDistances = urlDistances + "%2C" + origenesC[i].longitud + "%7C"
+            }
+            else{
+              urlDistances = urlDistances + origenesC[i].latitud
+              urlDistances = urlDistances + "%2C" + origenesC[i].longitud
+            }
           }
-          else{
-            urlDistances = urlDistances + response.data.results[0].geometry.location.lat
-            urlDistances = urlDistances + "%2C" + response.data.results[0].geometry.location.lng
+          else {
+            destinosC[y] = {
+              latitud: response.data.results[0].geometry.location.lat,
+              longitud: response.data.results[0].geometry.location.lng,
+              identificadorCliente: destino.identificadorCliente
+            }
+            if(destinos[y+1]!==undefined)  
+            {
+              urlDistances = urlDistances + response.data.results[0].geometry.location.lat
+              urlDistances = urlDistances + "%2C" + response.data.results[0].geometry.location.lng + "%7C"
+            }
+            else{
+              urlDistances = urlDistances + response.data.results[0].geometry.location.lat
+              urlDistances = urlDistances + "%2C" + response.data.results[0].geometry.location.lng
+            }
+            console.log("Destino " + y + ": " + destinosC[y].latitud + ", " + destinosC[y].longitud)
           }
-          
         } catch(err){
             return {
-              mensaje: "Error al obtener COORDENADAS, Valide",
-              error: err
+              mensaje: "ERROR 1: Error al obtener COORDENADAS de la dirección" + cadena5 + ", Valide",
+              error: err.toString() 
             }
         }
       }
 
       /*DESPUES DE QUE TENGAMOS TODOS LOS DESTINOS HACEMOS LA LLAMADA A LA API DE DISTANCE MATRIX*/
-      console.log("Destino " + y + ": " + destinosC[y].latitud + ", " + destinosC[y].longitud)
     }
+    //console.log(urlDistances)
     destinosFC[i] = destinosC
     countDistancias++
     var urlDistancesMatrix = `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${urlDistances}&key=${googleAPIKey}`
@@ -412,10 +486,11 @@ async function getDistanceWT(req){
             url: urlDistancesMatrix,
             json: true
         })
+        //console.log(response.data)
         distanciasFinal[i] = response.data
     } catch(err){
           return {
-            mensaje: "Error al obtener DISTANCIAS, Valide",
+            mensaje: "ERROR 2: Error al obtener DISTANCIAS, Valide",
             error: err
           }
     }
@@ -424,22 +499,56 @@ async function getDistanceWT(req){
   var distancias = []
   for(var i=0; i< distanciasFinal.length; i++)
   {
-    var origen = {
-      direccion: distanciasFinal[i].origin_addresses,
-      latitud: origenesC[i].latitud.toString(),
-      longitud: origenesC[i].longitud.toString(),
-      identificadorCliente:origenesC[i].identificadorCliente.toString()
+    if(distanciasFinal[i].origin_addresses.toString() === "0,0")
+    {
+      var origen = {
+        direccion: "ERROR AL OBTENER DIRECCION",
+        latitud: "ERROR",
+        longitud: "ERROR",
+        identificadorCliente:origenesC[i].identificadorCliente.toString()
+      }
     }
+    else {
+      var origen = {
+        direccion: distanciasFinal[i].origin_addresses,
+        latitud: origenesC[i].latitud.toString(),
+        longitud: origenesC[i].longitud.toString(),
+        identificadorCliente:origenesC[i].identificadorCliente.toString()
+      }
+    }
+    
     var destinos = []
     for(var j=0; j< distanciasFinal[i].destination_addresses.length; j++)
     {
-      
-      destinos[j] = {
-        direccion: distanciasFinal[i].destination_addresses[j],
-        latitud: destinosFC[i][j].latitud.toString(),
-        longitud: destinosFC[i][j].longitud.toString(),
-        distancia: distanciasFinal[i].rows[0].elements[j].distance.text,
-        identificadorCliente: destinosFC[i][j].identificadorCliente.toString()
+      console.log(distanciasFinal[i].rows[0].elements[j])
+      if(distanciasFinal[i].rows[0].elements[j].status === "ZERO_RESULTS")
+      {
+        destinos[j] = {
+          direccion: distanciasFinal[i].destination_addresses[j],
+          latitud: destinosFC[i][j].latitud.toString(),
+          longitud: destinosFC[i][j].longitud.toString(),
+          distancia: "ERROR EN DISTANCIA",
+          identificadorCliente: destinosFC[i][j].identificadorCliente.toString()
+        }
+      }
+      else if(distanciasFinal[i].rows[0].elements[j].distance.text === "1 m")
+      {
+        destinos[j] = {
+          direccion: distanciasFinal[i].destination_addresses[j],
+          latitud: destinosFC[i][j].latitud.toString(),
+          longitud: destinosFC[i][j].longitud.toString(),
+          distancia: "ERROR EN DISTANCIA",
+          identificadorCliente: destinosFC[i][j].identificadorCliente.toString()
+        }
+      }
+      else {
+        destinos[j] = {
+          direccion: distanciasFinal[i].destination_addresses[j],
+          latitud: destinosFC[i][j].latitud.toString(),
+          longitud: destinosFC[i][j].longitud.toString(),
+          distancia: distanciasFinal[i].rows[0].elements[j].distance.text,
+          identificadorCliente: destinosFC[i][j].identificadorCliente.toString()
+        }
       }
     }
     distancias[i] = {
@@ -450,6 +559,8 @@ async function getDistanceWT(req){
 
   console.log("Se realizaron " + countCoordenadas + " llamadas a la API de Geocode")
   console.log("Se realizaron " + countDistancias + " llamadas a la API de Distance Matrix")
+
+  console.log(distancias)
 
   return {
       data: {
